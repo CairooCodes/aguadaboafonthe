31ea2ecf02e40d38614db00830b11b412507ed53
Cairo <cairofelipedev@gmail.com>
2021-03-20 19:03:41 -0300

    Header completed

--- index.php
+++ index.php
@@ -38,12 +38,12 @@ include './admin/lead-insert.php';
 
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-
-      <a href="index.html" class="logo back-to-top d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
-      </a>
-
+    <div class="container-fluid container-xl d-flex justify-content-between">
+      <div>
+        <a href="index.html" class="logo back-to-top d-flex align-items-center">
+          <img src="assets/img/logo.png" alt="">
+        </a>
+      </div>
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
@@ -65,12 +65,13 @@ include './admin/lead-insert.php';
               <li><a href="#">Drop Down 4</a></li>
             </ul>
           </li>
-          <li><a class="getstarted scrollto" href="#about">Contato</a></li>
         </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
       </nav>
+      <div>
+        <a class="getstarted scrollto back-to-top" href="#about">Get Started</a>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </div>
       <!-- .navbar -->
-
     </div>
   </header>
   <!-- End Header -->
@@ -111,6 +112,7 @@ include './admin/lead-insert.php';
 
           <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
             <div class="content">
+
               <h3>Who We Are</h3>
               <h2>Expedita voluptas omnis cupiditate totam eveniet nobis sint iste. Dolores est repellat corrupti reprehenderit.</h2>
               <p>
